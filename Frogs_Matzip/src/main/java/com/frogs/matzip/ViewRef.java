b9a2c40bb14bbefe5451eb94bf957d45e9f84369
Park-cheolmin <pcm9307@naver.com>
2020-10-20 16:23:01 +0900

    20.10.20. 글등록 관리자만 접속 가능 구현완료

--- Frogs_Matzip/src/main/java/com/frogs/matzip/ViewRef.java
+++ Frogs_Matzip/src/main/java/com/frogs/matzip/ViewRef.java
@@ -1,6 +1,10 @@
 package com.frogs.matzip;
 
 public class ViewRef {
+	
+	public static final String URI_USER = "user";
+	public static final String URI_REST = "rest";
+	
 	public static final String TEMP = "template/base_template";
 	public static final String LIST_MAP = "rest/list_map";
 }
